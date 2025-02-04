159f811690f9bdbbdf1b4cc2a6d65377fbaa563c
John Marc Balbada <91250096+jmarcbalbada@users.noreply.github.com>
2025-02-05 03:24:29 +0800

    added encrypt decrypt feature

--- BalbadaFileEncryptDecrypt/Form1.Designer.cs
+++ BalbadaFileEncryptDecrypt/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -46,34 +48,59 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(106, 96);
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(40, 84);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "ENCRYPT";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(506, 96);
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(194, 84);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "DECRYPT";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(61, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "BALBADA, JOHN MARC - CS446 - F1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "(.enc)";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(298, 158);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "File Encrypt using AES";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,6 +110,8 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- BalbadaFileEncryptDecrypt/Form1.cs
+++ BalbadaFileEncryptDecrypt/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,28 +14,129 @@ namespace BalbadaFileEncryptDecrypt
 {
     public partial class Form1 : Form
     {
+        // api key, for more secure we can add it to .env or windows manager, but for demo we hardcode it here
+        private static string SecretKey = "x-balbada-47GjX9tLzQk82mYF6Cdw5P"; 
+
         public Form1()
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
         }
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
-
+            string filePath = openFileDialog1.FileName;
+            bool isEncrypted = EncryptFile(filePath);
+            if (isEncrypted)
+            {
+               MessageBox.Show($"File encrypted: {filePath}.enc", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
         {
-
+            string filePath = openFileDialog2.FileName;
+            bool isDecrypted = DecryptFile(filePath);
+            if (isDecrypted)
+            {
+               MessageBox.Show($"File decrypted: {filePath.Replace(".enc", "")}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            openFileDialog1.ShowDialog();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            openFileDialog2.ShowDialog();
+        }
+
+        // encrypt uses AES
+        private bool EncryptFile(string filePath)
+        {
+            try
+            {
+                byte[] keyBytes = Encoding.UTF8.GetBytes(SecretKey);
+                byte[] iv = new byte[16];
+                using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+                {
+                    rng.GetBytes(iv);
+                }
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = keyBytes;
+                    aes.IV = iv;
+                    aes.Mode = CipherMode.CBC;
+                    aes.Padding = PaddingMode.PKCS7;
+
+                    using (FileStream fs = new FileStream(filePath + ".enc", FileMode.Create))
+                    {
+                        fs.Write(iv, 0, iv.Length); 
+
+                        using (CryptoStream cs = new CryptoStream(fs, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                        using (FileStream fsInput = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                        {
+                            fsInput.CopyTo(cs);
+                        }
+                    }
+                }
+
+                // delete the file after encrypting
+                File.Delete(filePath);
+                return true;
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine(err.ToString());
+                MessageBox.Show("Something went wrong!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false;
+            }
+            
+        }
+
+        // decrypt, encryptApi == decryptApi
+        private bool DecryptFile(string filePath)
+        {
+            try
+            {
+                byte[] keyBytes = Encoding.UTF8.GetBytes(SecretKey);
+
+                using (FileStream fsInput = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] iv = new byte[16];
+                    fsInput.Read(iv, 0, iv.Length);
+
+                    using (Aes aes = Aes.Create())
+                    {
+                        aes.Key = keyBytes;
+                        aes.IV = iv;
+                        aes.Mode = CipherMode.CBC;
+                        aes.Padding = PaddingMode.PKCS7;
+
+                        using (CryptoStream cs = new CryptoStream(fsInput, aes.CreateDecryptor(), CryptoStreamMode.Read))
+
+                        // revert to non .enc or orig file ext
+                        using (FileStream fsOutput = new FileStream(filePath.Replace(".enc", ""), FileMode.Create))
+                        {
+                            cs.CopyTo(fsOutput);
+                        }
+                    }
+                }
+
+                // delete the enc file after decrypting
+                File.Delete(filePath);
+                return true;
+
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine(err.ToString());
+                MessageBox.Show("Invalid API key!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false;
+            }
 
         }
     }
